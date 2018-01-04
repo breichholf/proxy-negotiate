ddb4b0aaafd427495b3b83d7c981724d40e154f8
Yannick CANN <yannick.cann@sagem.com>
2018-01-04 13:27:10 +0100

    Add support to Win32
    Use winkerberos module instead of gssapi on Win32 platform

--- proxy_negotiate.py
+++ proxy_negotiate.py
@@ -1,9 +1,13 @@
 import base64
 import gevent
-import winkerberos as kerberos
 import os
 import socket
 import sys
+if sys.platform == "win32":
+    import winkerberos
+else:
+    import gssapi
+    import fcntl
 
 from logging import getLogger
 from gevent.server import StreamServer
@@ -23,7 +27,34 @@ def forward(src, dst):
     finally:
         src.close()
 
+def forward_stdin(sock):
+    if sys.platform != "win32":
+        # set stdin to non-blocking so we can read available bytes
+        fd = sys.stdin.fileno()
+        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
+    try:
+        while True:
+            wait_read(sys.stdin.fileno())
+            data = sys.stdin.read()
+            if not data:
+                break
+            sock.sendall(data)
+    finally:
+        sock.close()
+
+def forward_stdout(sock):
+    try:
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                break
+            sys.stdout.write(data)
+            sys.stdout.flush()
+    finally:
+        sock.close()
+ 
 class NegotiateProxy(StreamServer):
     def __init__(self, listener, upstream, **kwargs):
         super(NegotiateProxy, self).__init__(listener, **kwargs)
@@ -36,10 +67,16 @@ class NegotiateProxy(StreamServer):
             data += src.recv(1024)
             if b'\r\n\r\n' in data:
                 break
-
-        status, ctx = kerberos.authGSSClientInit('HTTP/%s' % self.upstream[0], gssflags=0, mech_oid=kerberos.GSS_MECH_OID_KRB5)
-        status = kerberos.authGSSClientStep(ctx, "")
-        b64token = kerberos.authGSSClientResponse(ctx)
+                
+        if sys.platform == "win32":
+            status, ctx = winkerberos.authGSSClientInit('HTTP/%s' % self.upstream[0], gssflags=0, mech_oid=kerberos.GSS_MECH_OID_KRB5)
+            status = winkerberos.authGSSClientStep(ctx, "")
+            b64token = winkerberos.authGSSClientResponse(ctx)
+        else:
+            service = gssapi.Name('HTTP@%s' % self.upstream[0], gssapi.NameType.hostbased_service)
+            ctx = gssapi.SecurityContext(name=service, usage='initiate')
+            token = ctx.step()
+            b64token = base64.b64encode(token).encode('ascii')
 
         headers, data = data.split(b'\r\n\r\n', 1)
         headers = headers.split('\r\n')
@@ -62,3 +99,61 @@ class NegotiateProxy(StreamServer):
 
         gevent.joinall(forwarders)
 
+def netcat(host, port, proxy_host, proxy_port):
+    request = [('CONNECT %s:%d HTTP/1.1' % (host, port)).encode('ascii')]
+    request.append(('Host: %s:%d' % (host, port)).encode('ascii'))
+    request.append(('Proxy-Connection: Keep-Alive').encode('ascii'))
+    request.append(('\r\n').encode('ascii'))
+
+    dst = create_connection((proxy_host, proxy_port))
+    dst.sendall(b'\r\n'.join(request))
+
+    data = b''
+    while True:
+        data += dst.recv(1024)
+        if b'\r\n\r\n' in data:
+            break
+
+    if b'200 Connection established' not in data and b'407' in data:
+        if sys.platform == "win32":
+            status, ctx = winkerberos.authGSSClientInit('HTTP/%s' % proxy_host, gssflags=0, mech_oid=kerberos.GSS_MECH_OID_KRB5)
+            status = winkerberos.authGSSClientStep(ctx, "")
+            b64token = winkerberos.authGSSClientResponse(ctx)
+        else:
+            service = gssapi.Name('HTTP@%s' % proxy_host, gssapi.NameType.hostbased_service)
+            ctx = gssapi.SecurityContext(name=service, usage='initiate')
+            token = ctx.step()
+            b64token = base64.b64encode(token).encode('ascii')
+        request[-1] = ('Proxy-Authorization: Negotiate %s' % b64token).encode('ascii')
+
+        request.append(('\r\n').encode('ascii'))
+
+        try:
+            dst.sendall(b'\r\n'.join(request))
+        except:
+            # if proxy does not support Keep-Alive
+            dst.close()
+            dst = create_connection((proxy_host, proxy_port))
+            dst.sendall(b'\r\n'.join(request))
+
+        data = b''
+        while True:
+            data += dst.recv(1024)
+            if b'\r\n\r\n' in data:
+                break
+
+    if b'200 Connection established' in data:
+        sys.stderr.write('Proxy connection established\n')
+        data = data.split(b'\r\n\r\n', 1)[1]
+        if data:
+            dst.sendall(data)
+
+        forwarders = (gevent.spawn(forward_stdin, dst),
+                      gevent.spawn(forward_stdout, dst))
+
+        gevent.joinall(forwarders)
+    elif b'407' in data:
+        sys.stderr.write('Proxy authentication failed\n')
+    else:
+        version, status_code, status_message = data.split(b'\r\n', 1)[0].split(b' ', 2)
+        sys.stderr.write('Proxy returned %s %s\n' % (status_code, status_message))
