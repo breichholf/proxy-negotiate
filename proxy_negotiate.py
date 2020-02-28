8f766e54dbc2be88effce1e184c886a702c5a2f7
breichholf <brian.reichholf@gmail.com>
2020-02-28 17:43:43 +0100

    Move out token fetching and clean up byte formatting

--- proxy_negotiate.py
+++ proxy_negotiate.py
@@ -3,19 +3,20 @@ import gevent
 import os
 import socket
 import sys
-if sys.platform == "win32":
-    import winkerberos
+
+if sys.platform == 'win32':
+    import winkerberos as kerberos
 else:
     import gssapi
     import fcntl
 
 from logging import getLogger
+
 from gevent.server import StreamServer
 from gevent.socket import create_connection, wait_read
 
 logger = getLogger(__name__)
 
-__version__ = '1.0.0'
 
 def forward(src, dst):
     try:
@@ -27,8 +28,9 @@ def forward(src, dst):
     finally:
         src.close()
 
+
 def forward_stdin(sock):
-    if sys.platform != "win32":
+    if sys.platform != 'win32':
         # set stdin to non-blocking so we can read available bytes
         fd = sys.stdin.fileno()
         fl = fcntl.fcntl(fd, fcntl.F_GETFL)
@@ -44,6 +46,7 @@ def forward_stdin(sock):
     finally:
         sock.close()
 
+
 def forward_stdout(sock):
     try:
         while True:
@@ -54,42 +57,38 @@ def forward_stdout(sock):
             sys.stdout.flush()
     finally:
         sock.close()
- 
+
+
 class NegotiateProxy(StreamServer):
+
     def __init__(self, listener, upstream, **kwargs):
-        super(NegotiateProxy, self).__init__(listener, **kwargs)
+        super().__init__(listener, **kwargs)
 
         self.upstream = upstream
 
     def handle(self, src, addr):
-        data = b''
+        data = bytearray()
         while True:
             data += src.recv(1024)
             if b'\r\n\r\n' in data:
                 break
-                
-        if sys.platform == "win32":
-            status, ctx = winkerberos.authGSSClientInit('HTTP/%s' % self.upstream[0], gssflags=0, mech_oid=winkerberos.GSS_MECH_OID_KRB5)
-            status = winkerberos.authGSSClientStep(ctx, "")
-            b64token = winkerberos.authGSSClientResponse(ctx)
-        else:
-            service = gssapi.Name('HTTP@%s' % self.upstream[0], gssapi.NameType.hostbased_service)
-            ctx = gssapi.SecurityContext(name=service, usage='initiate')
-            token = ctx.step()
-            b64token = base64.b64encode(token).encode('ascii')
+
+        log(f'{addr[0]}:{addr[1]} accepted')
+
+        krb_token = get_krb_token(self.upstream[0])
 
         headers, data = data.split(b'\r\n\r\n', 1)
-        headers = headers.split('\r\n')
+        headers = headers.split(b'\r\n')
 
         replaced = False
         for i, header in enumerate(headers):
-            if header.startswith('Proxy-Authorization:'):
-                headers[i] = b'Proxy-Authorization: Negotiate %s' % b64token
+            if header.startswith(b'Proxy-Authorization:'):
+                headers[i] = f'Proxy-Authorization: Negotiate {krb_token}'.encode()
                 replaced = True
                 break
 
         if not replaced:
-            headers.append(b'Proxy-Authorization: Negotiate %s' % b64token)
+            headers.append(f'Proxy-Authorization: Negotiate {krb_token}'.encode())
 
         dst = create_connection(self.upstream)
         dst.sendall(b'\r\n'.join(headers) + b'\r\n\r\n' + data)
@@ -99,34 +98,27 @@ class NegotiateProxy(StreamServer):
 
         gevent.joinall(forwarders)
 
+
 def netcat(host, port, proxy_host, proxy_port):
-    request = [('CONNECT %s:%d HTTP/1.1' % (host, port)).encode('ascii')]
-    request.append(('Host: %s:%d' % (host, port)).encode('ascii'))
-    request.append(('Proxy-Connection: Keep-Alive').encode('ascii'))
-    request.append(('\r\n').encode('ascii'))
+    request = list(f'CONNECT {host}:{port} HTTP/1.1'.encode())
+    request.append(f'Host: {host}:{port}'.encode())
+    request.append(b'Proxy-Connection: Keep-Alive')
+    request.append(b'\r\n')
 
     dst = create_connection((proxy_host, proxy_port))
     dst.sendall(b'\r\n'.join(request))
 
-    data = b''
+    data = bytearray()
     while True:
         data += dst.recv(1024)
         if b'\r\n\r\n' in data:
             break
 
     if b'200 Connection established' not in data and b'407' in data:
-        if sys.platform == "win32":
-            status, ctx = winkerberos.authGSSClientInit('HTTP/%s' % proxy_host, gssflags=0, mech_oid=winkerberos.GSS_MECH_OID_KRB5)
-            status = winkerberos.authGSSClientStep(ctx, "")
-            b64token = winkerberos.authGSSClientResponse(ctx)
-        else:
-            service = gssapi.Name('HTTP@%s' % proxy_host, gssapi.NameType.hostbased_service)
-            ctx = gssapi.SecurityContext(name=service, usage='initiate')
-            token = ctx.step()
-            b64token = base64.b64encode(token).encode('ascii')
-        request[-1] = ('Proxy-Authorization: Negotiate %s' % b64token).encode('ascii')
+        krb_token = get_krb_token(proxy_host)
+        request[-1] = f'Proxy-Authorization: Negotiate {krb_token}'.encode()
 
-        request.append(('\r\n').encode('ascii'))
+        request.append(b'\r\n')
 
         try:
             dst.sendall(b'\r\n'.join(request))
@@ -136,7 +128,7 @@ def netcat(host, port, proxy_host, proxy_port):
             dst = create_connection((proxy_host, proxy_port))
             dst.sendall(b'\r\n'.join(request))
 
-        data = b''
+        data = bytearray()
         while True:
             data += dst.recv(1024)
             if b'\r\n\r\n' in data:
@@ -155,5 +147,30 @@ def netcat(host, port, proxy_host, proxy_port):
     elif b'407' in data:
         sys.stderr.write('Proxy authentication failed\n')
     else:
-        version, status_code, status_message = data.split(b'\r\n', 1)[0].split(b' ', 2)
-        sys.stderr.write('Proxy returned %s %s\n' % (status_code, status_message))
+        version, status_code, status_message = (
+            data.split(b'\r\n', 1)[0].split(b' ', 2)
+        )
+        sys.stderr.write(f'Proxy returned {status_code} {status_message}\n')
+
+
+def log(message, *args):
+    message = message % args
+    sys.stderr.write(message + '\n')
+
+
+def get_krb_token(host):
+    if sys.platform == 'win32':
+        # previously used with parameters `gssflags=0` and
+        # `mech_oid=kerberos.GSS_MECH_OID_KRB5`
+        # neither are required.
+        status, ctx = kerberos.authGSSClientInit(f'HTTP/{host}')
+        _ = kerberos.authGSSClientStep(ctx, '')
+        krb_token = kerberos.authGSSClientResponse(ctx)
+    else:
+        service = gssapi.Name(f'HTTP@{host}',
+                              gssapi.NameType.hostbased_service)
+        ctx = gssapi.SecurityContext(name=service, usage='initiate')
+        token = ctx.step()
+        krb_token = base64.b64encode(token)
+
+    return krb_token
